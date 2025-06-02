3e9d07e70b45210cb8240864a9c4c0c263ae74ce
uinstinct <61635505+uinstinct@users.noreply.github.com>
2025-06-02 17:08:15 +0530

    add vitest

---
+++ core/vitest.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from "vitest/config";
+
+export default defineConfig({
+  test: {
+    testTransformMode: {
+      web: ["/.[jt]s?$/"],
+      ssr: ["/.[jt]s?$/"],
+    },
+    globalSetup: "./test/vitest.global-setup.ts",
+    setupFiles: "./test/vitest.setup.ts",
+  },
+});
